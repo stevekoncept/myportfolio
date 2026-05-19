23a813efd3c1dcf565dc509cd765568af4dc0800
Stevetech24 <madubuikeebere203@gmail.com>
2026-05-19 16:11:45 +0100

    edited code

--- script.js
+++ script.js
@@ -6,30 +6,24 @@ const skillsBtn = document.querySelector(".skills-btn");
 const projectsBtn = document.querySelector(".projects-btn");
 const contactBtn = document.querySelector(".contact-btn");
 const respHireBtn = document.querySelector(".resp-hire-btn");
-
-
-//
 openMenu.addEventListener("click", () => {
   sideResponsiveBar.style.transform = " translateX(0)";
   openMenu.style.display = "none";
   closeMenu.style.display = "block";
   document.body.classList.toggle("no-scroll");
 });
-
 window.addEventListener("resize", () => {
   if (window.innerWidth > 480) {
     openMenu.style.display = "none";
     closeMenu.style.display = "none";
   }
 });
-
 closeMenu.addEventListener("click", () => {
   sideResponsiveBar.style.transform = " translateX(-500px)";
   closeMenu.style.display = "none";
   openMenu.style.display = "block";
   document.body.classList.remove("no-scroll");
 });
-
 const removeScroll = function () {
   document.body.classList.remove("no-scroll");
   sideResponsiveBar.style.transform = " translateX(-500px)";
