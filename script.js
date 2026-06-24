be55d43909bcead3e60255f5027ad6056cd73fe3
Stevetech24 <madubuikeebere203@gmail.com>
2026-06-24 14:28:32 +0100

    updated

--- script.js
+++ script.js
@@ -25,8 +25,6 @@ sections.forEach((section) => {
 
 openMenu.addEventListener("click", () => {
   sideResponsiveBar.style.transform = " translateX(0)";
-  openMenu.style.display = "none";
-  closeMenu.style.display = "block";
   document.body.classList.toggle("no-scroll");
 });
 window.addEventListener("resize", () => {
@@ -38,15 +36,11 @@ window.addEventListener("resize", () => {
 //
 closeMenu.addEventListener("click", () => {
   sideResponsiveBar.style.transform = " translateX(-500px)";
-  closeMenu.style.display = "none";
-  openMenu.style.display = "block";
   document.body.classList.remove("no-scroll");
 });
 const removeScroll = function () {
   document.body.classList.remove("no-scroll");
   sideResponsiveBar.style.transform = " translateX(-500px)";
-  closeMenu.style.display = "none";
-  openMenu.style.display = "block";
 };
 aboutBtn.addEventListener("click", removeScroll);
 skillsBtn.addEventListener("click", removeScroll);
